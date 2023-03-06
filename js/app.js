7402b1e6356c012a1298201d31b082d3d2a59358
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-06 09:51:57 +0600

    [ADD]-get-city-temperature-with-city-name

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
+// free api keys
 const API_KEY = `5fd24e836da6064cfbb23ada0d94f6cb`;
 
-
+// Load temperature data form API
 
 const loadTemperature = async (city) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
@@ -12,12 +13,37 @@ const loadTemperature = async (city) => {
 
 
 
-const displayTemperature = data => {
-    console.log(data.main.temp);
+// Display temperature data in UI
 
-    const temperature = document.getElementById("temperature");
+const displayTemperature = (data) => {
+  console.log(data.main.temp);
 
-    temperature.innerText = data.main.temp;
-}
+  const temperature = document.getElementById("temperature");
+  temperature.innerText = data.main.temp;
+};
 
+
+
+
+// add evenlintner in the search button
+document.getElementById("search-btn").addEventListener("click", function () {
+
+  // get input field  value
+  const inputField = document.getElementById("search-field");
+  const inputFieldValue = inputField.value;
+
+  // get city name in UI
+  const cityName = document.getElementById("city-name");
+  cityName.innerText = inputFieldValue;
+
+  // call the loadTemperature function with inputFieldValue parameter
+  loadTemperature(inputFieldValue);
+
+  // clear the input field
+  inputField.value = "";
+});
+
+
+
+// call loadTemperature function
 loadTemperature("Seoul");
